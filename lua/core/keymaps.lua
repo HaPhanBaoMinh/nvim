d87fdb6e483c8b5d0a2bca36c3a77639a6c17e83
baominh <haphanbaominh9674@gmail.com>
2025-01-23 16:25:12 +0700

    Update files

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,18 @@
+-- Navigate vim panes better
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+vim.keymap.set("n", "<F2>", function()
+  if vim.wo.relativenumber then
+    vim.wo.relativenumber = false
+    vim.wo.number = true
+  else
+    vim.wo.relativenumber = true
+  end
+end, { desc = "Toggle relative line numbers" })
+
+
