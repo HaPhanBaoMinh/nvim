a19318c7a4543de180b22b908488260b9d331d2e
minh.ha <minh.ha@opswat.com>
2025-08-12 00:00:06 +0700

    update

--- init.lua
+++ init.lua
@@ -6,7 +6,9 @@
 -- auto install vim-plug and plugins, if not found
 local data_dir = vim.fn.stdpath('data')
 if vim.fn.empty(vim.fn.glob(data_dir .. '/site/autoload/plug.vim')) == 1 then
-	vim.cmd('silent !curl -fLo ' .. data_dir .. '/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
+	vim.cmd('silent !curl -fLo ' ..
+		data_dir ..
+		'/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim')
 	vim.o.runtimepath = vim.o.runtimepath
 	vim.cmd('autocmd VimEnter * PlugInstall --sync | source $MYVIMRC')
 end
@@ -18,32 +20,35 @@ vim.g.start_time = vim.fn.reltime()
 vim.loader.enable() --  SPEEEEEEEEEEED 
 vim.call('plug#begin')
 
-Plug('catppuccin/nvim', { ['as'] = 'catppuccin' }) --colorscheme
+Plug('catppuccin/nvim', { ['as'] = 'catppuccin' })       --colorscheme
 Plug('ellisonleao/gruvbox.nvim', { ['as'] = 'gruvbox' }) --colorscheme 2
-Plug('uZer/pywal16.nvim', { [ 'as' ] = 'pywal16' }) --or, pywal colorscheme
-Plug('nvim-lualine/lualine.nvim') --statusline
-Plug('nvim-tree/nvim-web-devicons') --pretty icons
-Plug('folke/which-key.nvim') --mappings popup
-Plug('romgrk/barbar.nvim') --bufferline
-Plug('goolord/alpha-nvim') --pretty startup
-Plug('nvim-treesitter/nvim-treesitter') --improved syntax
-Plug('mfussenegger/nvim-lint') --async linter
-Plug('nvim-tree/nvim-tree.lua') --file explorer
-Plug('windwp/nvim-autopairs') --autopairs 
-Plug('lewis6991/gitsigns.nvim') --git
-Plug('numToStr/Comment.nvim') --easier comments
-Plug('norcalli/nvim-colorizer.lua') --color highlight
-Plug('ibhagwan/fzf-lua') --fuzzy finder and grep
-Plug('numToStr/FTerm.nvim') --floating terminal
-Plug('ron-rs/ron.vim') --ron syntax highlighting
-Plug('MeanderingProgrammer/render-markdown.nvim') --render md inline
-Plug('emmanueltouzery/decisive.nvim') --view csv files
-Plug('folke/twilight.nvim') --surrounding dim
-Plug('folke/flash.nvim', { ['branch'] = 'main' }) --flash search
-Plug('nvim-lua/plenary.nvim') --required by many plugins
-Plug('mason-org/mason.nvim') --package manager
-Plug('mason-org/mason-lspconfig.nvim') --mason lsp config
-Plug('neovim/nvim-lspconfig') --lsp config
+Plug('uZer/pywal16.nvim', { ['as'] = 'pywal16' })        --or, pywal colorscheme
+Plug('nvim-lualine/lualine.nvim')                        --statusline
+Plug('nvim-tree/nvim-web-devicons')                      --pretty icons
+Plug('folke/which-key.nvim')                             --mappings popup
+Plug('romgrk/barbar.nvim')                               --bufferline
+Plug('goolord/alpha-nvim')                               --pretty startup
+Plug('nvim-treesitter/nvim-treesitter')                  --improved syntax
+Plug('mfussenegger/nvim-lint')                           --async linter
+Plug('nvim-tree/nvim-tree.lua')                          --file explorer
+Plug('windwp/nvim-autopairs')                            --autopairs
+Plug('lewis6991/gitsigns.nvim')                          --git
+Plug('numToStr/Comment.nvim')                            --easier comments
+Plug('norcalli/nvim-colorizer.lua')                      --color highlight
+Plug('ibhagwan/fzf-lua')                                 --fuzzy finder and grep
+Plug('numToStr/FTerm.nvim')                              --floating terminal
+Plug('ron-rs/ron.vim')                                   --ron syntax highlighting
+Plug('MeanderingProgrammer/render-markdown.nvim')        --render md inline
+Plug('emmanueltouzery/decisive.nvim')                    --view csv files
+Plug('folke/twilight.nvim')                              --surrounding dim
+Plug('folke/flash.nvim', { ['branch'] = 'main' })        --flash search
+Plug('nvim-lua/plenary.nvim')                            --required by many plugins
+Plug('mason-org/mason.nvim')                             --package manager
+Plug('mason-org/mason-lspconfig.nvim')                   --mason lsp config
+Plug('neovim/nvim-lspconfig')                            --lsp config
+Plug('rafamadriz/friendly-snippets')                     --snippets
+Plug('L3MON4D3/LuaSnip', { "tag': 'v2.*" })              --snippets engine
+Plug('saghen/blink.cmp', { ['tag'] = 'v0.11.2' })        --completion engine
 
 vim.call('plug#end')
 
@@ -66,23 +71,24 @@ require("plugins.lualine")
 require("plugins.nvim-lint")
 -- require("plugins.nvim-tree")
 require("plugins.render-markdown")
--- require("plugins.treesitter")
+require("plugins.treesitter")
+require("plugins.blink")
 -- require("plugins.twilight")
 -- require("plugins.which-key")
 
-vim.defer_fn(function() 
-		--defer non-essential configs,
-		--purely for experimental purposes:
-		--this only makes a difference of +-10ms on initial startup
-require("plugins.autopairs")
-require("plugins.fterm")
-require("plugins.fzf-lua")
-require("plugins.nvim-tree")
-require("plugins.treesitter")
-require("plugins.twilight")
-require("plugins.which-key")
-require("plugins.mason")
-require("plugins.lspconfig")
+vim.defer_fn(function()
+	--defer non-essential configs,
+	--purely for experimental purposes:
+	--this only makes a difference of +-10ms on initial startup
+	require("plugins.autopairs")
+	require("plugins.fterm")
+	require("plugins.fzf-lua")
+	require("plugins.nvim-tree")
+	require("plugins.treesitter")
+	require("plugins.twilight")
+	require("plugins.which-key")
+	require("plugins.mason")
+	require("plugins.lspconfig")
 end, 100)
 
 load_theme()
